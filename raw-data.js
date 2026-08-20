5e0677e61b168fa8e6fdc776cb0c759f0c17c3a9
Sarada <saradawrites@gmail.com>
2026-08-20 22:45:01 +0530

    Update RAW_SCRIPT_URL to new Google script URL

--- raw-data.js
+++ raw-data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const RAW_SCRIPT_URL =
-    'https://script.google.com/macros/s/AKfycbwUmwkrx1wI1NrFDm6Bsvbcr0SCHSgkKTheN7_fLeVec4-w15D1k_11T8OQ4q4eV3bH5w/exec';
+    'https://script.google.com/macros/s/AKfycbz6D0molucbp24l1aw_4vjpZDk-RuBH6-NmoENgLXGmWjjeCXnFBFKOmb7b2jn-b5KVpA/exec';
 
   const RAW_FRONTEND_VERSION = '15.6';
 
